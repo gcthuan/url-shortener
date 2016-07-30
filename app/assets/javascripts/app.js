6f9c02f98b951cda2b42adcc99b14c42c28ccb98
gcthuan <gcthuan118@gmail.com>
2016-07-29 19:36:12 -0700

    Added navbar and random background

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,13 @@
 var urlShortener = angular.module("urlShortener", []);
 urlShortener.controller("MainCtrl", ["$scope", function ($scope) {
 	$scope.test = "hello world";
+	$scope.backgroundImg = getABackgroundImg();
 }]);
+
+function getABackgroundImg() {
+	return "background/" + getRandomIntInclusive(1, 17) + ".png";
+}
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
