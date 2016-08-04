28c635a36acf023a48b190ca0170e268afa470a7
gcthuan <gcthuan118@gmail.com>
2016-08-04 13:49:41 -0700

    Moved copy to clipboard button to the right in result box

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,17 +1,20 @@
-var urlShortener = angular.module('urlShortener', ['ngclipboard']);
-urlShortener.controller("MainCtrl", ["$scope", "$http", "$location", function ($scope, $http, $location) {
+var urlShortener = angular.module('urlShortener', ['ngclipboard'], function($compileProvider) {
+	$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|localhost):/);
+});
+urlShortener.config(['$locationProvider', function($locationProvider) {
+	$locationProvider.html5Mode(true);
+}]);
+urlShortener.controller("MainCtrl", ["$scope", "$http", "$location", "$window", function ($scope, $http, $location, $window) {
 	$scope.backgroundImg = getABackgroundImg();
 	$scope.getShortenedUrl = function(url) {
 		return $http.post('/shortener/shorten', {'url': url}).then(function(response) {
-			$scope.shortened_url = $location.host() + ":" + $location.port() + "/" + response.data.shortened_url;
+			$scope.shortened_url = $location.protocol() + "://" + $location.host() + ":" + $location.port() + "/" + response.data.shortened_url;
 			$scope.click_count = response.data.click_count;
 			return;
 		});
 	};
 	// $scope.visitUrl = function(url) {
-	// 	return $http.get('/shortener/visit?url=' + url).then(function(response) {
-	// 		return;
-	// 	});
+	// 	$window.location.href = url;
 	// };
 }]);
 
