718f09f884850988cf2d7f37f1687a95b9029e57
gcthuan <gcthuan118@gmail.com>
2016-08-17 16:22:21 -0700

    Added packages.config

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'httparty'
 # parse html
 gem 'oga'
 
+gem 'puma'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,7 @@ if ENV["REDIS_PRODUCTION_URL"]
 	uri = URI.parse(ENV["REDIS_PRODUCTION_URL"])
 	REDIS = Redis.new(host: uri.host, port: uri.port, timeout: 5)
 else
-	REDIS = Redis.new("redis://localhost:6379/")
+	REDIS = Redis.new(url: "redis://localhost:6379/")
 end
 
 REDIS.setnx("counter", 2729)
