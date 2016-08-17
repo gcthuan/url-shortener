db6776161162cd1a8f6624d68572ca8119c9dc04
gcthuan <gcthuan118@gmail.com>
2016-08-16 19:03:53 -0700

    Changed redis production url config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,9 @@
 require "redis"
-url = ENV["REDIS_PRODUCTION_URL"] || "redis://localhost:6379/"
-REDIS = Redis.new(url: url)
+if ENV["REDIS_PRODUCTION_URL"]
+	uri = URI.parse(ENV["REDIS_PRODUCTION_URL"])
+	REDIS = Redis.new(host: uri.host, port: uri.port, timeout: 5)
+else
+	REDIS = Redis.new("redis://localhost:6379/")
+end
+
 REDIS.setnx("counter", 2729)
